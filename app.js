0169c281d436010b01d374c1443a07c02b623fe2
el hiho <hihi.cod@gmail.com>
2020-12-06 15:12:57 +0100

    version 0.3 agregando pre-loader

---
+++ app.js
@@ -0,0 +1,8 @@
+// Function para que se vaya el loader
+
+
+
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.preload');
+    preload.classList.add('preload-finish');
+});
